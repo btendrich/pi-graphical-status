5a3009829d9ad31ef2380ad4035aaad3bca2535e
Brendan Tendrich <brendan@tendrich.net>
2019-01-11 14:31:53 -0500

    size testing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,66 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    updateTimer = new QTimer(this);
+    connect(updateTimer, SIGNAL( timeout()), this, SLOT(updateTimerTimeout()));
+    updateTimer->start(1000);
+
+    ntpProcess.setProgram("/usr/bin/ntpstat");
+
+    eth0Process.setProgram("/sbin/ifconfig");
+    eth0Process.setArguments(QStringList("en0"));
+
+    wlan0Process.setProgram("/sbin/ifconfig");
+    wlan0Process.setArguments(QStringList("en1"));
+
+    wProcess.setProgram("/usr/bin/w");
+
+    connect(&ntpProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(readNtpOutput()));
+    connect(&eth0Process, SIGNAL(readyReadStandardOutput()), this, SLOT(readEth0Output()));
+    connect(&wlan0Process, SIGNAL(readyReadStandardOutput()), this, SLOT(readWlan0Output()));
+    connect(&wProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(readWOutput()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateTimerTimeout()
+{
+    ntpProcess.kill();
+    ntpProcess.start();
+
+    wlan0Process.kill();
+    wlan0Process.start();
+
+    eth0Process.kill();
+    eth0Process.start();
+
+    wProcess.kill();
+    wProcess.start();
+}
+
+void MainWindow::readNtpOutput()
+{
+    QString string = ntpProcess.readAllStandardOutput();
+    ui->ntpstat_textedit->setPlainText(string);
+}
+
+void MainWindow::readEth0Output()
+{
+    QString string = eth0Process.readAllStandardOutput();
+    ui->eth0_textedit->setPlainText(string);
+}
+
+void MainWindow::readWlan0Output()
+{
+    QString string = wlan0Process.readAllStandardOutput();
+    ui->wlan0_textedit->setPlainText(string);
+}
+
+void MainWindow::readWOutput()
+{
+    QString string = wProcess.readAllStandardOutput();
+    ui->uptime_textedit->setPlainText(string);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDateTime>
+#include <QTimer>
+#include <QProcess>
+#include <QDebug>
+#include <QRegularExpression>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +22,20 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void updateTimerTimeout();
+    void readNtpOutput();
+    void readEth0Output();
+    void readWlan0Output();
+    void readWOutput();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *updateTimer;
+    QProcess ntpProcess;
+    QProcess eth0Process;
+    QProcess wlan0Process;
+    QProcess wProcess;
 };
 
 #endif // MAINWINDOW_H
